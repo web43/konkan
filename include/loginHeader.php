f95b1e18a9a348a81fc116503a0b6b61f7f4284a
web43 <web@neuleaflifespace.com>
2019-12-26 14:43:01 +0530

    project added

---
+++ include/loginHeader.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Konkan Trails</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="keywords" content="">
+    <meta name="description" content="Konkan trails">
+     <link rel="apple-touch-icon" sizes="180x180" href="assets\images\icons\favicon.ico">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets\images\icons\favicon.ico">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets\images\icons\favicon.ico">
+    <link rel="shortcut icon" href="assets\images\icons\favicon.ico">
+    <!-- Plugins CSS File -->
+    <link rel="stylesheet" href="assets\css\bootstrap.min.css">
+    <!-- Main CSS File -->
+    <link rel="stylesheet" href="assets\css\style.css">
+</head>
